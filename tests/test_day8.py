b90e47d687e289f5938d8bf0fae1d5e8246307a8
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-04-02 18:15:29 +0200

    feat: we can parse nodes by line, and we can extract the direction header
    Co-Authored-By: Stefan Boos <kontakt@boos.systems>

--- tests/test_day8.py
+++ tests/test_day8.py
@@ -1,5 +1,43 @@
+import re
+
+
+def get_directions_header(test_string):
+    newline_position = test_string.find("\n")
+
+    return test_string[:newline_position]
+
+
+
+
+class Node:
+    def __init__(self, name, left, right) -> None:
+        self.name = name
+        self.left = left
+        self.right = right
+
+    @staticmethod
+    def from_line (input_string):
+        match = re.search("(\w+) = \((\w+), (\w+)\)", input_string)
+        return Node(match.group(1), match.group(2), match.group(3))
+
 class TestDay8:
 
-    def test_something(self):
-        assert True == False
+    def test_get_directions_header(self):
+        test_string = """RLL
+        
+        AAA = (BBB, ZZZ)
+        """
+        result = get_directions_header(test_string)
+        assert result == "RLL"
 
+    def test_created_node_has_correct_name(self):
+        result = Node.from_line("AAA = (BBB, ZZZ)")
+        assert result.name == "AAA"
+
+    def test_created_node_has_correct_left(self):
+        result = Node.from_line("AAA = (BBB, ZZZ)")
+        assert result.left == "BBB"
+
+    def test_created_node_has_correct_right(self):
+        result = Node.from_line("AAA = (BBB, ZZZ)")
+        assert result.right == "ZZZ"
