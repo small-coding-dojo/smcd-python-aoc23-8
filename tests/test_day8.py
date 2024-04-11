6cae0657579cf6c537541daffa2b575d0df80612
Stefan Boos <kontakt@boos.systems>
2024-04-11 18:03:02 +0200

    feat: We can parse a network of one instruction and one node
    Co-authored-by: Holger Janssen-Kroll <hjanssenkroll@igus.net>
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>

--- tests/test_day8.py
+++ tests/test_day8.py
@@ -1,14 +1,6 @@
 import re
 
 
-def get_directions_header(test_string):
-    newline_position = test_string.find("\n")
-
-    return test_string[:newline_position]
-
-
-
-
 class Node:
     def __init__(self, name, left, right) -> None:
         self.name = name
@@ -16,9 +8,33 @@ class Node:
         self.right = right
 
     @staticmethod
-    def from_line (input_string):
+    def from_line(input_string):
         match = re.search("(\w+) = \((\w+), (\w+)\)", input_string)
         return Node(match.group(1), match.group(2), match.group(3))
+
+
+class Network:
+    def __init__(self, header):
+        self.header = header
+        self.nodes = []
+
+    @staticmethod
+    def from_input(text):
+        network = Network(Network.get_directions_header(text))
+        nodes = [Node.from_line(Network.get_network_body(text))]
+        network.nodes = nodes
+        return network
+
+    @staticmethod
+    def get_network_body(text):
+        return text[text.find("\n") + 1:]
+
+    @staticmethod
+    def get_directions_header(test_string):
+        newline_position = test_string.find("\n")
+
+        return test_string[:newline_position]
+
 
 class TestDay8:
 
@@ -27,7 +43,7 @@ class TestDay8:
         
         AAA = (BBB, ZZZ)
         """
-        result = get_directions_header(test_string)
+        result = Network.get_directions_header(test_string)
         assert result == "RLL"
 
     def test_created_node_has_correct_name(self):
@@ -41,3 +57,25 @@ class TestDay8:
     def test_created_node_has_correct_right(self):
         result = Node.from_line("AAA = (BBB, ZZZ)")
         assert result.right == "ZZZ"
+
+    def test_parsing_simple_input_delivers_one_node_network(self):
+        result = Network.from_input("""RLL
+        
+        AAA = (BBB, ZZZ)
+        """)
+        assert result.header == "RLL"
+        assert len(result.nodes) == 1
+        assert result.nodes[0].name == "AAA"
+        assert result.nodes[0].left == "BBB"
+        assert result.nodes[0].right == "ZZZ"
+
+    def test_parsing_simple_input_delivers_one_node_network2(self):
+        result = Network.from_input("""L
+
+        CCC = (ZZZ, GGG)
+        """)
+        assert result.header == "L"
+        assert len(result.nodes) == 1
+        assert result.nodes[0].name == "CCC"
+        assert result.nodes[0].left == "ZZZ"
+        assert result.nodes[0].right == "GGG"
